faec1e2ea9099953b936538fefcc3a4bc8ced644
fred <my@email.com>
2019-01-24 16:36:42 +0000

    Changes to work with latest rev of click board

--- spi-LedringR-click.py
+++ spi-LedringR-click.py
@@ -6,19 +6,19 @@ import spidev
 # of the Ultra96 mikro click mezzanine board 
 
 # Obtain the reset pin of slot 1 of the Ultra96 mikro mezzanine board
-# PS pin MIO37 is connected to slot 1 RST
-# PS pin MIO40 is connected to slot 2 RST
+# PS pin EMIO2 is connected to slot 1 RST
+# PS pin EMIO3 is connected to slot 2 RST
 
 # Linux pin number to Xilinx pin numbers are weird and have a large
 # base number than can change between different releases of Linux
 # The pynq base fcn will help here!
-mio_linux_number = GPIO.get_gpio_base() + 37
+#mio_linux_number = GPIO.get_gpio_base() + 37
 
 # EMIOs start after MIO and there
 # is fixed offset for ZYNQ (54) and ZYNQ US+ (78)
-#   emio_linux_number = GPIO.get_gpio_pin(emio_offset)
+emio_linux_number = GPIO.get_gpio_base() + 78 + 2
 
-rst_pin = GPIO(mio_linux_number, 'out')
+rst_pin = GPIO(emio_linux_number, 'out')
 
 # Reset is usually active LOW
 rst_pin.write(1)
@@ -30,7 +30,7 @@ rst_pin.write(1)
 print("Make the Click Led ring R board work!")
 
 spi = spidev.SpiDev()
-spi.open(0, 0)
+spi.open(1, 0)
 
 # This Click board is just (4) 74HCP595 8-bit shift
 # registers daisy chained to the SPI data bus.
